f3d50e4257147c77551f09f25792c5e4465370e7
JTemp21 <90410284+JTemp21@users.noreply.github.com>
2022-11-14 14:36:06 -0500

    Update Program.cs

--- CIS129FinalProject/Program.cs
+++ CIS129FinalProject/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.text;
+using System.Threading.Tasks;
+
+namespace WizertFiveGame
+{
+    public class Wizert
+    {
+        private string _wizertName;
+        private int _hp;
+        private int _mp;
+
+        public Wizert(string wizertName, int hp, int mp)
+        {
+            _wizertName = wizertName;
+            _hp = hp;
+            _mp = mp;
+        }
+
+        public void useMagic()
+        {
+            if (_mp <= 0)
+            {
+                Console.WriteLine("Out of magic points.")
+            }
+            else
+            {
+                _mp--;
+            }
+        }
+
+        public void TakeDamage()
+        {
+            _hp--;
+
+            if (_hp <= 0)
+            {
+                Console.WriteLine($"The Wizert {_wizertName} has died in battle!")
+            }
+            else
+                Console.WriteLine($"The Wizert {_wizertName} has taken damage.")
+        }
+    }
+}
