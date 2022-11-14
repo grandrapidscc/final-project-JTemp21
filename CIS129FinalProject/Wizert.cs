3d55bbb60a7a1f3b71086f984ca04749ced6cb93
JTemp21 <90410284+JTemp21@users.noreply.github.com>
2022-11-14 14:51:31 -0500

    class wizert

---
+++ CIS129FinalProject/Wizert.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS129FinalProject
+{
+    public class Wizert
+    {
+        private string _wizertName;
+        private int _hp;
+        private int _mp;
+
+        public Wizert(string wizertName, int hp, int mp)
+        {
+            _wizertName = wizertName;
+            _hp = hp;
+            _mp = mp;
+        }
+
+        public void useMagic()
+        {
+            if (_mp <= 0)
+            {
+                Console.WriteLine("Out of magic points.");
+            }
+            else
+            {
+                _mp--;
+            }
+        }
+
+        public void TakeDamage()
+        {
+            _hp--;
+
+            if (_hp <= 0)
+            {
+                Console.WriteLine($"The Wizert {_wizertName} has died in battle!");
+            }
+            else
+                Console.WriteLine($"The Wizert {_wizertName} has taken damage.");
+        }
+    }
+}
